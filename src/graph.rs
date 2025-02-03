ba0a95616ccf8f03ee59161f688107958050c4ff
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2025-02-03 10:33:28 -0600

    [feat] add examples graph
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- src/graph.rs
+++ src/graph.rs
@@ -1,14 +1,8 @@
+#[derive(Debug)]
 pub struct Graph {
     nodes: Vec<usize>,
     adjacency: Vec<usize>,
 }
-
-
-pub struct SubGraph<'a> {
-    nodes: Vec<usize>,
-    adjacency: &'a Vec<usize>,
-}
-
 
 impl Graph {
     pub fn new(input:(Vec<usize>, Vec<(usize,usize)>)) -> Self {
@@ -25,27 +19,4 @@ impl Graph {
 
         Self { nodes, adjacency }
     }
-    
-    pub fn iter_subgraph(self) -> impl Iterator<Item = SubGraph> {
-        let mut state: usize = 0;
-        let max_state: usize = 1 << self.edges.len();
-        return std::iter::from_fn(move || {
-            if state >= max_state {
-                return None;
-            }
-            let nodes: Vec<usize> = self
-                .nodes
-                .iter()
-                .enumerate()
-                .filter(|(i, _)| (state >> i) & 1 == 1)
-                .map(|(_, item)| item.clone())
-                .collect();
-
-            let subgraph = SubGraph { nodes, &self.adjacency };
-            state += 1;
-            return Some(subgraph);
-        });
-    }
 }
-
-pub fn is_clique(graph: Graph) -> bool {}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 mod graph;
+mod examples;
 
 fn main() {
-    let cliques:Vec<graph::Graph> = graph.iter_subgraph()
-    .filter(|subgraph| graph::is_clique(subgraph))
-    .collect()
-    
+
 }
